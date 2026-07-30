08fb7d2acd04b6f9d2b7390ab02fc182b71e3524
David García <adavidgr13@gmail.com>
2026-07-30 15:43:09 +0200

    added CharacterDetailScreen and loading skeleton

--- app/src/main/java/com/example/pokerick/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/pokerick/ui/navigation/AppNavigation.kt
@@ -1,9 +1,12 @@
 package com.example.pokerick.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pokerick.ui.screens.detail.CharacterDetailScreen
 import com.example.pokerick.ui.screens.list.CharacterListScreen
 
 @Composable
@@ -15,7 +18,20 @@ fun AppNavigation() {
         composable("character_list") {
             CharacterListScreen(
                 onNavigateToDetail = { characterId ->
-                    // to detail
+                    navController.navigate("character_detail/$characterId")
+                }
+            )
+        }
+
+        composable(
+            route = "character_detail/{characterId}",
+            arguments = listOf(
+                navArgument("characterId") { type = NavType.StringType }
+            )
+        ) {
+            CharacterDetailScreen(
+                onNavigateBack = {
+                    navController.popBackStack()
                 }
             )
         }
